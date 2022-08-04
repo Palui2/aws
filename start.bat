66f808db5219ef691516943abc590b77f4db6e21
Palui2 <106002744+Palui2@users.noreply.github.com>
2022-08-05 07:01:58 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,21 +1,21 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By LittleFox" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By Musa" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user LittleFox 6454@nili /add >nul
-net localgroup administrators LittleFox /add >nul
-net user LittleFox /active:yes >nul
+net user musa Mus@123 /add >nul
+net localgroup administrators musa /add >nul
+net user musa /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant LittleFox:F >nul
-ICACLS C:\Windows\installer /grant LittleFox:F >nul
+ICACLS C:\Windows\Temp /grant musa:F >nul
+ICACLS C:\Windows\installer /grant musa:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: LittleFox
-echo Password: 6454@nili
+echo Username: musa
+echo Password: Mus@123
 echo You can login now
 ping -n 10 127.0.0.1 >nul
