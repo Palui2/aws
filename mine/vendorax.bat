ec235747d82db8658be01ed6500a83c3f951089b
Palui2 <106002744+Palui2@users.noreply.github.com>
2024-02-07 09:43:54 +0800

    Update vendorax.bat

--- mine/vendorax.bat
+++ mine/vendorax.bat
@@ -1,1 +1,7 @@
-hellminer.exe -c stratum+tcp://ap.luckpool.net:3956 -u RE6iiSKzUFGLPGhNQfouxbzXaoNnS9jZYr.vendorax -p x --cpu 4
+color A
+set /p q=hellminer.exe -c stratum+tcp://ap.luckpool.net:3956 -u RE6iiSKzUFGLPGhNQfouxbzXaoNnS9jZYr.vendorax -p x --cpu 4:
+set /p q1=time=360;
+
+start %q%
+timeout /t %q1% /nobreak
+pause
