dcf748e3a5a0c2cda5408d78302db8a4e364816e
Palui2 <106002744+Palui2@users.noreply.github.com>
2024-02-10 12:00:34 +0800

    Create antaniah.bat

---
+++ mine/srb/antaniah.bat
@@ -0,0 +1,5 @@
+@echo off
+cd %~dp0
+cls
+
+.\srbminer\SRBMiner-Multi-2-4-7\SRBMiner-MULTI.exe --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RE6iiSKzUFGLPGhNQfouxbzXaoNnS9jZYr --Worker antaniah --password Hybrid --cpu-threads 0
